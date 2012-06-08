3f1e0c160ccff117a6fa4de5261454e6e7cc8e3a
DisposaBoy <disposaboy@dby.me>
2012-06-08 22:19:51 +0100

    - complete impl

--- ac_imports.go
+++ ac_imports.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bytes"
 	"go/ast"
 	"go/parser"
-	"go/printer"
 	"go/token"
 	"strings"
 )
@@ -14,21 +12,24 @@ type ImportDecl struct {
 	Path string `json:"path"`
 }
 
+type ImportDeclArg struct {
+	Name string `json:"name"`
+	Path string `json:"path"`
+	Add  bool   `json:"add"`
+}
+
 type ImportsResult struct {
-	FileImports  []ImportDecl `json:"file_imports"`
-	ImportPaths  []string     `json:"import_paths"`
-	Src          string       `json:"src"`
-	SizeRef      int          `json:"size_ref"`
-	LineRef      int          `json:"line_ref"`
-	LineCountRef int          `json:"line_count_ref"`
+	Imports []ImportDecl `json:"imports"`
+	Src     string       `json:"src"`
+	LineRef int          `json:"line_ref"`
 }
 
 type ImportsArgs struct {
-	Fn          string            `json:"fn"`
-	Src         string            `json:"src"`
-	ImportPaths bool              `json:"import_paths"`
-	Toggle      []ImportDecl      `json:"toggle"`
-	Env         map[string]string `json:"env"`
+	Fn        string          `json:"fn"`
+	Src       string          `json:"src"`
+	Toggle    []ImportDeclArg `json:"toggle"`
+	TabWidth  int             `json:"tab_width"`
+	TabIndent bool            `json:"tab_indent"`
 }
 
 func unquote(s string) string {
@@ -45,105 +46,133 @@ func init() {
 		Doc:  "",
 		Func: func(r Request) (data, error) {
 			res := ImportsResult{
-				FileImports: []ImportDecl{},
-				ImportPaths: []string{},
+				Imports: []ImportDecl{},
 			}
 
 			a := ImportsArgs{
-				Toggle: []ImportDecl{},
-				Env:    map[string]string{},
+				Toggle:    []ImportDeclArg{},
+				TabWidth:  8,
+				TabIndent: true,
 			}
 
 			if err := r.Decode(&a); err != nil {
 				return res, err
 			}
 
-			if a.ImportPaths {
-				res.ImportPaths, _ = importPaths(a.Env)
-			}
-
 			fset, af, err := parseAstFile(a.Fn, a.Src, parser.ImportsOnly|parser.ParseComments)
 			if err == nil {
-				tf := fset.File(af.Pos())
-				res.LineCountRef = tf.LineCount()
-
-				for _, v := range af.Decls {
-					if p := fset.Position(v.End()); p.IsValid() {
-						res.LineRef = maxInt(res.LineRef, p.Line)
-						res.SizeRef = maxInt(res.SizeRef, p.Offset)
-					}
-				}
-				for _, v := range af.Comments {
-					if p := fset.Position(v.End()); p.IsValid() {
-						res.LineRef = maxInt(res.LineRef, p.Line)
-						res.SizeRef = maxInt(res.SizeRef, p.Offset)
-					}
-				}
-
-				toggle := map[ImportDecl]bool{}
-				for _, v := range a.Toggle {
-					toggle[v] = true
-				}
-
-				var first *ast.GenDecl
-				for j := 0; j < len(af.Decls); j += 1 {
-					d := af.Decls[j]
-					if decl, ok := d.(*ast.GenDecl); ok {
-						if decl.Tok == token.IMPORT {
-							for i := 0; i < len(decl.Specs); i += 1 {
-								if sp, ok := decl.Specs[i].(*ast.ImportSpec); ok {
-									id := ImportDecl{Path: unquote(sp.Path.Value)}
-									if sp.Name != nil {
-										id.Name = sp.Name.String()
-									}
-									if _, ok := toggle[id]; ok {
-										delete(toggle, id)
-										decl.Specs = append(decl.Specs[:i], decl.Specs[i+1:]...)
-									} else {
-										res.FileImports = append(res.FileImports, id)
-									}
-								}
-							}
-							if len(decl.Specs) == 0 && decl.Lparen == token.NoPos {
-								af.Decls = append(af.Decls[:j], af.Decls[j+1:]...)
-							} else if first == nil {
-								first = decl
-							}
-						}
-					}
-				}
-
-				if len(toggle) > 0 {
-					if first == nil {
-						first = &ast.GenDecl{
-							Tok:    token.IMPORT,
-							Lparen: 1,
-						}
-						af.Decls = append(af.Decls, first)
-					} else if first.Lparen == token.NoPos {
-						first.Lparen = 1
-					}
-					for id, _ := range toggle {
-						sp := &ast.ImportSpec{
-							Path: &ast.BasicLit{
-								Value: quote(id.Path),
-								Kind:  token.STRING,
-							},
-						}
-						first.Specs = append(first.Specs, sp)
-						res.FileImports = append(res.FileImports, id)
-					}
-				}
-				ast.SortImports(fset, af)
-
-				if len(a.Toggle) > 0 {
-					buf := &bytes.Buffer{}
-					if err = printer.Fprint(buf, fset, af); err == nil {
-						res.Src = buf.String()
-					}
+				if len(af.Decls) > 0 {
+					res.LineRef = fset.Position(af.Decls[len(af.Decls)-1].End()).Line
+					af, res.Imports = imp(fset, af, a.Toggle)
+					res.Src, err = printSrc(fset, af, a.TabIndent, a.TabWidth)
 				}
 			}
 			return res, err
 		},
 	})
+}
+
+func imp(fset *token.FileSet, af *ast.File, toggle []ImportDeclArg) (*ast.File, []ImportDecl) {
+	add := map[ImportDecl]bool{}
+	del := map[ImportDecl]bool{}
+	for _, sda := range toggle {
+		sd := ImportDecl{
+			Path: sda.Path,
+			Name: sda.Name,
+		}
+		if sda.Add {
+			add[sd] = true
+		} else {
+			del[sd] = true
+		}
+	}
+
+	var firstDecl *ast.GenDecl
+	imports := map[ImportDecl]bool{}
+	for _, decl := range af.Decls {
+		if gdecl, ok := decl.(*ast.GenDecl); ok && len(gdecl.Specs) > 0 {
+			hasC := false
+			sj := 0
+			for _, spec := range gdecl.Specs {
+				if ispec, ok := spec.(*ast.ImportSpec); ok {
+					sd := ImportDecl{
+						Path: unquote(ispec.Path.Value),
+					}
+					if ispec.Name != nil {
+						sd.Name = ispec.Name.String()
+					}
+
+					if sd.Path == "C" {
+						hasC = true
+					} else if del[sd] {
+						if sj > 0 {
+							if lspec, ok := gdecl.Specs[sj-1].(*ast.ImportSpec); ok {
+								lspec.EndPos = ispec.Pos()
+							}
+						}
+						continue
+					} else {
+						imports[sd] = true
+					}
+				}
+
+				gdecl.Specs[sj] = spec
+				sj += 1
+			}
+			gdecl.Specs = gdecl.Specs[:sj]
+
+			if !hasC && firstDecl == nil {
+				firstDecl = gdecl
+			}
+		}
+	}
+
+	if len(add) > 0 {
+		if firstDecl == nil {
+			firstDecl = &ast.GenDecl{
+				Tok:    token.IMPORT,
+				Lparen: 1,
+			}
+			af.Decls = append(af.Decls, firstDecl)
+		} else if firstDecl.Lparen == token.NoPos {
+			firstDecl.Lparen = 1
+		}
+
+		for sd, _ := range add {
+			if !imports[sd] {
+				ispec := &ast.ImportSpec{
+					Path: &ast.BasicLit{
+						Value: quote(sd.Path),
+						Kind:  token.STRING,
+					},
+				}
+				if sd.Name != "" {
+					ispec.Name = &ast.Ident{
+						Name: sd.Name,
+					}
+				}
+				firstDecl.Specs = append(firstDecl.Specs, ispec)
+				imports[sd] = true
+			}
+		}
+	}
+
+	dj := 0
+	for _, decl := range af.Decls {
+		if gdecl, ok := decl.(*ast.GenDecl); ok {
+			if len(gdecl.Specs) == 0 {
+				continue
+			}
+		}
+		af.Decls[dj] = decl
+		dj += 1
+	}
+	af.Decls = af.Decls[:dj]
+
+	ast.SortImports(fset, af)
+	res := []ImportDecl{}
+	for sd, _ := range imports {
+		res = append(res, sd)
+	}
+	return af, res
 }
