6e5109b0631a851d08dcb317b4d3622ddd3b3eea
DisposaBoy <disposaboy@dby.me>
2012-06-30 09:58:22 +0100

    - return declarations for the whole package - exclude blank id

--- ac_declarations.go
+++ ac_declarations.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"go/ast"
 	"go/printer"
+	"path/filepath"
 )
 
 type DeclarationsArgs struct {
@@ -23,72 +24,81 @@ func init() {
 				return decls, err
 			}
 
-			fset, af, err := parseAstFile(a.Fn, a.Src, 0)
+			fset, _, err := parseAstFile(a.Fn, a.Src, 0)
 			if err == nil {
-				for _, d := range af.Decls {
-					if p := fset.Position(d.Pos()); p.IsValid() {
-						switch n := d.(type) {
-						case *ast.FuncDecl:
-							decl := map[string]interface{}{
-								"name":     n.Name.Name,
-								"kind":     "func",
-								"doc":      n.Doc.Text(),
-								"filename": p.Filename,
-								"line":     p.Line,
-								"column":   p.Column,
-							}
-							if n.Recv != nil {
-								recvFields := n.Recv.List
-								if len(recvFields) == 0 {
-									break
-								}
-								typ := recvFields[0].Type
-								buf := bytes.NewBuffer([]byte("("))
-								if printer.Fprint(buf, fset, typ) != nil {
-									break
-								}
-								fmt.Fprintf(buf, ").%s", n.Name.Name)
-								decl["name"] = buf.String()
-							}
-							decls = append(decls, decl)
-						case *ast.GenDecl:
-							for _, spec := range n.Specs {
-								switch gn := spec.(type) {
-								case *ast.TypeSpec:
-									if vp := fset.Position(gn.Pos()); vp.IsValid() {
-										decls = append(decls, map[string]interface{}{
-											"name":     gn.Name.Name,
-											"kind":     "type",
-											"doc":      gn.Doc.Text(),
-											"filename": vp.Filename,
-											"line":     vp.Line,
-											"column":   vp.Column,
-										})
+				pkg, err := parsePkg(fset, filepath.Dir(a.Fn), 0)
+				if pkg == nil {
+					return decls, err
+				}
+
+				for _, af := range pkg.Files {
+					for _, d := range af.Decls {
+						if p := fset.Position(d.Pos()); p.IsValid() {
+							switch n := d.(type) {
+							case *ast.FuncDecl:
+								if n.Name.Name != "_" {
+									decl := map[string]interface{}{
+										"name":     n.Name.Name,
+										"kind":     "func",
+										"doc":      n.Doc.Text(),
+										"filename": p.Filename,
+										"line":     p.Line,
+										"column":   p.Column,
 									}
-								case *ast.ValueSpec:
-									for _, v := range gn.Names {
-										if vp := fset.Position(v.Pos()); vp.IsValid() {
-											kind := ""
-											switch v.Obj.Kind {
-											case ast.Typ:
-												kind = "type"
-											case ast.Fun:
-												kind = "func"
-											case ast.Con:
-												kind = "const"
-											case ast.Var:
-												kind = "var"
-											default:
-												continue
-											}
+									if n.Recv != nil {
+										recvFields := n.Recv.List
+										if len(recvFields) == 0 {
+											break
+										}
+										typ := recvFields[0].Type
+										buf := bytes.NewBuffer([]byte("("))
+										if printer.Fprint(buf, fset, typ) != nil {
+											break
+										}
+										fmt.Fprintf(buf, ").%s", n.Name.Name)
+										decl["name"] = buf.String()
+									}
+									decls = append(decls, decl)
+								}
+							case *ast.GenDecl:
+								for _, spec := range n.Specs {
+									switch gn := spec.(type) {
+									case *ast.TypeSpec:
+										if vp := fset.Position(gn.Pos()); gn.Name.Name != "_" && vp.IsValid() {
 											decls = append(decls, map[string]interface{}{
-												"name":     v.Name,
-												"kind":     kind,
-												"doc":      "",
+												"name":     gn.Name.Name,
+												"kind":     "type",
+												"doc":      gn.Doc.Text(),
 												"filename": vp.Filename,
 												"line":     vp.Line,
 												"column":   vp.Column,
 											})
+										}
+									case *ast.ValueSpec:
+										for _, v := range gn.Names {
+											if vp := fset.Position(v.Pos()); v.Name != "_" && vp.IsValid() {
+												kind := ""
+												switch v.Obj.Kind {
+												case ast.Typ:
+													kind = "type"
+												case ast.Fun:
+													kind = "func"
+												case ast.Con:
+													kind = "const"
+												case ast.Var:
+													kind = "var"
+												default:
+													continue
+												}
+												decls = append(decls, map[string]interface{}{
+													"name":     v.Name,
+													"kind":     kind,
+													"doc":      "",
+													"filename": vp.Filename,
+													"line":     vp.Line,
+													"column":   vp.Column,
+												})
+											}
 										}
 									}
 								}
