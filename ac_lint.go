21013211257fb6fe6f129ce03b0d742e718bdb4e
DisposaBoy <disposaboy@dby.me>
2012-07-07 16:19:46 +0100

    - fix: search for flag.parse globally within the file

--- ac_lint.go
+++ ac_lint.go
@@ -49,44 +49,34 @@ func init() {
 }
 
 func lintCheckFlagParse(fset *token.FileSet, af *ast.File, res []AcLintReport) []AcLintReport {
+	reps := []AcLintReport{}
+	foundParse := false
 	ast.Inspect(af, func(node ast.Node) bool {
-		switch n := node.(type) {
-		case *ast.FuncDecl:
-			if n.Name.String() == "main" || n.Name.String() == "main" {
-				var flagCall *ast.CallExpr
-
-				ast.Inspect(node, func(node ast.Node) bool {
-					switch c := node.(type) {
-					case *ast.CallExpr:
-						if sel, ok := c.Fun.(*ast.SelectorExpr); ok {
-							if id, ok := sel.X.(*ast.Ident); ok && id.Name == "flag" {
-								if sel.Sel.String() == "Parse" {
-									flagCall = nil
-								} else {
-									flagCall = c
-								}
-							}
+		switch c := node.(type) {
+		case *ast.CallExpr:
+			if sel, ok := c.Fun.(*ast.SelectorExpr); ok {
+				if id, ok := sel.X.(*ast.Ident); ok && id.Name == "flag" {
+					if sel.Sel.String() == "Parse" {
+						foundParse = true
+					} else if !foundParse && c != nil {
+						tp := fset.Position(c.Pos())
+						if tp.IsValid() {
+							reps = append(reps, AcLintReport{
+								Row: tp.Line - 1,
+								Col: tp.Column - 1,
+								Msg: "Cannot find corresponding call to flag.Parse()",
+							})
 						}
 					}
-					return true
-				})
-
-				if flagCall != nil {
-					tp := fset.Position(flagCall.Pos())
-					s, _ := printSrc(fset, flagCall, true, 8)
-					res = append(res, AcLintReport{
-						Row: tp.Line - 1,
-						Col: tp.Column - 1,
-						Msg: "call " + s + " does not appear to have a corresponding call to flag.Parse()",
-					})
 				}
-
-				return false
 			}
 		}
-
-		return true
+		return !foundParse
 	})
+
+	if !foundParse {
+		res = append(res, reps...)
+	}
 
 	return res
 }
